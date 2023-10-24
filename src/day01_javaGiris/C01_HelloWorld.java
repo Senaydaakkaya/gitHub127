6e45a4bb1b79633aae980e75b00d5d71da4cf367
Senaydaakkaya <senaydaakkaya@gmail.com>
2023-10-24 13:39:35 +0300

    denemeson

--- src/day01_javaGiris/C01_HelloWorld.java
+++ src/day01_javaGiris/C01_HelloWorld.java
@@ -5,6 +5,7 @@ public class C01_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("hello world");
+        System.out.println("deneme");
     }
 
 
